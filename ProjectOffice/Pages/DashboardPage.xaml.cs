4aa6ace6647c1f39c5f64270626a16e9f457faa3
Tim <boba@gmail.com>
2024-05-14 14:02:39 +0300

    upd

---
+++ ProjectOffice/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace ProjectOffice.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : Page
+    {
+        SeriesCollection SeriesViews { get; set; }
+        public DashboardPage()
+        {
+            InitializeComponent();
+            LiveCharts.SeriesCollection seriesViews = new LiveCharts.SeriesCollection() {
+                new LineSeries
+                {
+                    Values = new LiveCharts.ChartValues<double>{1,3,4}
+                },
+                new ColumnSeries
+                {
+                    Values = new LiveCharts.ChartValues<double>{1,3,3,3,3,3}
+                }
+            };
+            SeriesViews = seriesViews;
+        }
+    }
+}
